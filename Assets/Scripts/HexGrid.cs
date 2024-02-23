97d293eaddb9511c4ebd0c689f7702f243485bf1
Skarlupka02 <Skarlupka02@gmail.com>
2024-02-24 00:00:20 +0300

    First commit HexagonalMap on color hexagonals

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using System;
+
+public class HexGrid : MonoBehaviour
+{
+    public int width = 6;
+    public int height = 6;
+
+    public HexCell cellPrefab;
+    public Text cellLabelPrefab;
+
+    public Color defaultColor = Color.white;
+    public Color touchedColor = Color.magenta;
+
+    HexCell[] cells;
+    Canvas gridCanvas;
+    HexMesh hexMesh;
+
+    private void Awake()
+    {
+        hexMesh= GetComponentInChildren<HexMesh>();
+        gridCanvas = GetComponentInChildren<Canvas>();
+
+        cells = new HexCell[width * height];
+        for (int z = 0, i = 0; z < height; z++){
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+    void CreateCell (int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
+        position.y = 0f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f); 
+
+        HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
+        cell.transform.SetParent(transform, false); //Делает родителем ячейки HexGrid
+        cell.transform.localPosition = position;
+        cell.coordinates = HexCoordinates.FromOffsetCoordinates(x, z);
+        cell.color = defaultColor;
+
+        Text label = Instantiate<Text>(cellLabelPrefab);
+        label.rectTransform.SetParent(gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = cell.coordinates.ToStringOnSeparateLines();
+    }
+
+    #region Косание ячеек
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) { HandleInput(); }
+    }
+    void HandleInput ()
+    {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit)) { TouchCell(hit.point); }
+    }
+    void TouchCell(Vector3 position)
+    {
+        position = transform.InverseTransformPoint(position);
+        HexCoordinates coordinates = HexCoordinates.FromPosition(position);
+        int index = coordinates.X + coordinates.Z * width + coordinates.Z / 2;
+        HexCell cell = cells[index];
+        if(cell.color == defaultColor) { cell.color = touchedColor; }
+        else { cell.color = defaultColor; }
+        hexMesh.Triangulate(cells);
+        Debug.Log("touch at" + position + "/n" + coordinates.ToString());
+    }
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        hexMesh.Triangulate(cells);
+    }
+}
