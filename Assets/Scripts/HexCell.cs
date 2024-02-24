d75d1c96d954081bda456f4c78d8668dcf179736
Skarlupka02 <Skarlupka02@gmail.com>
2024-02-25 00:01:22 +0300

    Add neighbors for cells

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -8,6 +8,19 @@ public class HexCell : MonoBehaviour
 
     public Color color;
 
+    [SerializeField]
+    HexCell[] neighbors;
+
+    public HexCell GetNeighbor (HexDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+    public void SetNeighbor(HexDirection direction, HexCell cell)
+    {
+        neighbors[(int)direction] = cell;
+        cell.neighbors[(int)direction.Opposite()] = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
