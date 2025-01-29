c9199dc314e67c53f028de5cab6cdd42a1c7ca6c
Skarlupka02 <Skarlupka02@gmail.com>
2025-01-29 13:51:37 +0300

    Moving vertices

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -338,9 +338,9 @@ public class HexMesh : MonoBehaviour
     void AddTriangle(Vector3 v1, Vector3 v2, Vector3 v3)
     {
         int vertexIndex = vertices.Count;
-        vertices.Add(v1);
-        vertices.Add(v2); 
-        vertices.Add(v3);
+        vertices.Add(Perturb(v1));
+        vertices.Add(Perturb(v2)); 
+        vertices.Add(Perturb(v3));
         triangles.Add(vertexIndex);
         triangles.Add(vertexIndex + 1);
         triangles.Add(vertexIndex + 2);
@@ -360,10 +360,10 @@ public class HexMesh : MonoBehaviour
     void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
     {
         int vertexIndex = vertices.Count;
-        vertices.Add((v1));
-        vertices.Add((v2));
-        vertices.Add((v3));
-        vertices.Add((v4));
+        vertices.Add(Perturb(v1));
+        vertices.Add(Perturb(v2));
+        vertices.Add(Perturb(v3));
+        vertices.Add(Perturb(v4));
         triangles.Add(vertexIndex);
         triangles.Add(vertexIndex + 2);
         triangles.Add(vertexIndex + 1);
@@ -384,5 +384,14 @@ public class HexMesh : MonoBehaviour
         colors.Add(c1);
         colors.Add(c2);
         colors.Add(c2);
+    }
+
+    Vector3 Perturb (Vector3 position)
+    {
+        Vector4 sample = HexMetrics.SampleNoise(position);
+        position.y += sample.y * 2f - 1f;
+        position.x += sample.x * 2f - 1f;
+        position.z += sample.z * 2f - 1f;
+        return position;
     }
 }
