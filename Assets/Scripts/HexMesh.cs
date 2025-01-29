93b67d5fbe1c8996c1f5370552547a1615cd610d
Skarlupka02 <Skarlupka02@gmail.com>
2025-01-29 14:47:37 +0300

    Subdivision of edge connections

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -67,52 +67,15 @@ public class HexMesh : MonoBehaviour
 
         if (direction == HexDirection.NE)
         {
-            TriangulateConnection(direction, cell, v1, v2);
+            TriangulateConnection(direction, cell, v1, e1, e2, v2);
         }
         if(direction <= HexDirection.SE)
         {
-            TriangulateConnection(direction, cell, v1, v2);
+            TriangulateConnection(direction, cell, v1, e1, e2, v2);
         }
 
-        //Vector3 v3 = center + HexMetrics.GetFirstCorner(direction);
-        //Vector3 v4 = center + HexMetrics.GetSecondCorner(direction);
-
-        //Vector3 bridge = HexMetrics.GetBridge(direction);
-        //Vector3 v3 = v1 + bridge;
-        //Vector3 v4 = v2 + bridge;
-
-        //AddQuad(v1, v2, v3, v4);
-
-        //HexCell prevNeighbor = cell.GetNeighbor(direction.Previous()) ?? cell;
-        //HexCell neighbor = cell.GetNeighbor(direction) ?? cell;
-        //HexCell nextNeighbor = cell.GetNeighbor(direction.Next()) ?? cell;
-
-        //AddQuadColor(cell.color,(cell.color + neighbor.color) * 0.5f);
-
-        //Color bridgeColor = (cell.color + neighbor.color) * 0.5f;
-        //AddQuadColor(cell.color, bridgeColor);
-
-        //AddTriangle(v1, center + HexMetrics.GetFirstCorner(direction), v3);
-        //AddTriangleColor(
-        //    cell.color,
-        //    (cell.color + prevNeighbor.color + neighbor.color) / 3f,
-        //    bridgeColor
-        //    );
-        //AddTriangle(v2, v4, center + HexMetrics.GetSecondCorner(direction));
-        //AddTriangleColor(
-        //    cell.color,
-        //    bridgeColor,
-        //    (cell.color + neighbor.color + nextNeighbor.color) / 3f
-        //    );
-        //AddQuadColor(
-        //    cell.color,
-        //    cell.color,
-        //    (cell.color + prevNeighbor.color + neighbor.color) / 3f,
-        //    (cell.color + neighbor.color + nextNeighbor.color) / 3f
-        //);
-
     }
-    void TriangulateConnection(HexDirection direction, HexCell cell, Vector3 v1, Vector3 v2)
+    void TriangulateConnection(HexDirection direction, HexCell cell, Vector3 v1, Vector3 e1, Vector3 e2, Vector3 v2)
     {
         HexCell neighbor = cell.GetNeighbor(direction);
         if (neighbor == null) { return; }
@@ -122,13 +85,20 @@ public class HexMesh : MonoBehaviour
         v3.y = v4.y = neighbor.Position.y;
         //v3.y = v4.y = neighbor.Elevation * HexMetrics.elevationStep;
 
+        Vector3 e3 = Vector3.Lerp(v3, v4, 1f / 3f);
+        Vector3 e4 = Vector3.Lerp(v3, v4, 2f / 3f);
+
         if (cell.GetEdgeType(direction) == HexEdgeType.Slope)
         {
             TriangulateEdgeTerraces(v1, v2, cell, v3, v4, neighbor);
         }
         else
         {
-            AddQuad(v1, v2, v3, v4);
+            AddQuad(v1, e1, v3, e3);
+            AddQuadColor(cell.color, neighbor.color);
+            AddQuad(e1, e2, e3, e4);
+            AddQuadColor(cell.color, neighbor.color);
+            AddQuad(e2, v2, e4, v4);
             AddQuadColor(cell.color, neighbor.color);
         }
 
