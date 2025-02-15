dc39c851e8699bcad951bd85ab0b9e198f4cb596
Skarlupka02 <Skarlupka02@gmail.com>
2025-02-15 16:29:57 +0300

    Zoom

--- Assets/Scripts/HexMapCamera.cs
+++ Assets/Scripts/HexMapCamera.cs
@@ -6,6 +6,9 @@ public class HexMapCamera : MonoBehaviour
 {
 
     Transform swivel, stick;
+    float zoom = 1f;
+    public float stickMinZoom, stickMaxZoom;
+    public float swivelMinZoom, swivelMaxZoom;
 
     private void Awake()
     {
@@ -21,6 +24,21 @@ public class HexMapCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float zoomDelta = Input.GetAxis("Mouse ScrollWheel");
+        if(zoomDelta != 0f)
+        {
+            AdjustZoom(zoomDelta);
+        }
+    }
+
+    void AdjustZoom(float delta)
+    {
+        zoom = Mathf.Clamp01(zoom + delta);
+
+        float distance = Mathf.Lerp(stickMinZoom, stickMaxZoom, zoom);
+        stick.localPosition = new Vector3(0f, 0f, distance);
+
+        float angle = Mathf.Lerp(swivelMinZoom,  swivelMaxZoom, zoom);
+        swivel.localRotation = Quaternion.Euler(angle, 0f, 0f);
     }
 }
