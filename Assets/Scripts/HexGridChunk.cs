397961c03d86f55d1c7ad5739662da0c1731bc32
Skarlupka02 <Skarlupka02@gmail.com>
2025-06-27 11:49:15 +0300

    The beginning and the end of the river

--- Assets/Scripts/HexGridChunk.cs
+++ Assets/Scripts/HexGridChunk.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
 
 public class HexGridChunk : MonoBehaviour
 {
@@ -432,6 +433,20 @@ public class HexGridChunk : MonoBehaviour
 
         TriangulateEdgeStrip(m, cell.Color, e, cell.Color);
         TriangulateEdgeFan(center, m, cell.Color);
+
+        bool reversed = cell.HasIncomingRiver;
+        TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, reversed);
+
+        center.y = m.v2.y = m.v4.y = cell.RiverSurfaceY;
+        rivers.AddTriangle(center, m.v2, m.v4); 
+        if (reversed)
+        {
+            rivers.AddTriangleUV(new Vector2(0.5f, 1f), new Vector2(1f, 0f), new Vector2(0f, 0f));
+        }
+        else
+        {
+            rivers.AddTriangleUV(new Vector2(0.5f, 0f), new Vector2(0f, 1f), new Vector2(1f, 1f));
+        }
     }
 
     void TriangulateAdjacentToRiver(HexDirection direction, HexCell cell, Vector3 center, EdgeVertices e)
@@ -471,6 +486,8 @@ public class HexGridChunk : MonoBehaviour
         {
             rivers.AddQuadUV(0f, 1f, 0f, 1f);
         }
+
+        
     }
 
     public void MeshTriangulateView()
