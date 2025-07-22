fe8e479facf7abb0af5affa14f48fda4f55cd829
Skarlupka02 <Skarlupka02@gmail.com>
2025-07-22 04:43:01 +0300

    Add: Adjusting the current at the mouth of the river

--- Assets/Scripts/HexGridChunk.cs
+++ Assets/Scripts/HexGridChunk.cs
@@ -804,9 +804,9 @@ public class HexGridChunk : MonoBehaviour
         estuaries.AddTriangleUV(new Vector2(0f, 0f), new Vector2(0f, 1f), new Vector2(0f, 1f));
         estuaries.AddQuadUV(0f, 0f, 0f, 1f);
 
-        estuaries.AddQuadUV2(new Vector2(1f, 0.8f), new Vector2(1f, 1.1f), new Vector2(1f, 0.8f), new Vector2(0.5f, 1.1f));
+        estuaries.AddQuadUV2(new Vector2(1.5f, 1f), new Vector2(0.7f, 1.15f), new Vector2(1f, 0.8f), new Vector2(0.5f, 1.1f));
         estuaries.AddTriangleUV2(new Vector2(0.5f, 1.1f), new Vector2(1f, 0.8f), new Vector2(0f, 0.8f));
-        estuaries.AddQuadUV2(new Vector2(0.5f, 1.1f), new Vector2(0f, 1.1f), new Vector2(0f, 0.8f), new Vector2(0f, 0.8f));
+        estuaries.AddQuadUV2(new Vector2(0.5f, 1.1f), new Vector2(0.3f, 1.15f), new Vector2(0f, 0.8f), new Vector2(-0.5f, 1f));
     }
     #endregion
 
