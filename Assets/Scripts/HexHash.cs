7ad8e33d399814997df7363125b37349e04668fc
Skarlupka02 <Skarlupka02@gmail.com>
2025-07-31 20:01:37 +0300

    Add: Added mixing of terrain feature types.

--- Assets/Scripts/HexHash.cs
+++ Assets/Scripts/HexHash.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public struct HexHash
 {
-    public float a, b, c;
+    public float a, b, c, d, e;
 
     public static HexHash Create()
     {
@@ -12,6 +12,8 @@ public struct HexHash
         hash.a = Random.value * 0.999f;
         hash.b = Random.value * 0.999f;
         hash.c = Random.value * 0.999f;
+        hash.d = Random.value * 0.999f;
+        hash.e = Random.value * 0.999f;
         return hash;
     }
 }
