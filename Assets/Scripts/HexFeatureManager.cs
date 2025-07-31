654e7a2bb78b0ed3957e2cedb0fd5121420d1512
Skarlupka02 <Skarlupka02@gmail.com>
2025-07-31 18:50:51 +0300

    Add: Adding a collection of prefabs

--- Assets/Scripts/HexFeatureManager.cs
+++ Assets/Scripts/HexFeatureManager.cs
@@ -6,7 +6,9 @@ using UnityEngine;
 public class HexFeatureManager : MonoBehaviour
 {
 
-    public Transform[] urbanPrefabs;
+    //public Transform[][] urbanPrefabs;
+    public HexFeatureCollection[] urbanCollections;
+
     Transform container;
 
     public void Clear() 
@@ -24,24 +26,22 @@ public class HexFeatureManager : MonoBehaviour
     public void AddFeature(HexCell cell, Vector3 position) 
     {
         HexHash hash = HexMetrics.SampleHashGrid(position);
-        //if(hash.a >= cell.UrbanLevel * 0.25f) return;
-        // Transform instance = Instantiate(urbanPrefabs[cell.UrbanLevel - 1]);
-        Transform prefab = PickPrefab(cell.UrbanLevel, hash.a);
+        Transform prefab = PickPrefab(cell.UrbanLevel, hash.a, hash.b);
         if (!prefab) return;
         Transform instance = Instantiate(prefab);
         position.y += instance.localScale.y * 0.5f;
         instance.localPosition = HexMetrics.Perturb(position);
-        instance.localRotation = Quaternion.Euler(0f, 360f * hash.b, 0f);
+        instance.localRotation = Quaternion.Euler(0f, 360f * hash.c, 0f);
         instance.SetParent(container, false);
     }
 
-    Transform PickPrefab (int level, float hash)
+    Transform PickPrefab (int level, float hash, float choice)
     {
         if(level > 0)
         {
             float[] thresholds = HexMetrics.GetFeatureThresholds(level - 1);
             for (int i = 0; i < thresholds.Length; i++) { 
-                if(hash < thresholds[i]) return urbanPrefabs[i];
+                if(hash < thresholds[i]) return urbanCollections[i].Pick(choice);
             }
         }
         return null;
